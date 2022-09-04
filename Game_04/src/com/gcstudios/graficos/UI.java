2091196ab3d025ff779ab5c3a2d8e24577f65c40
Gallamion <112811519+Gallamion@users.noreply.github.com>
2022-09-04 10:37:12 -0300

    Add files via upload

---
+++ Game_04/src/com/gcstudios/graficos/UI.java
@@ -0,0 +1,25 @@
+package com.gcstudios.graficos;
+
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+
+import com.gcstudios.entities.Player;
+import com.gcstudios.main.Game;
+
+public class UI {
+
+	public void render(Graphics g) {
+	
+		g.setColor(Color.red);
+		g.fillRect(10, 20,250 , 30);
+		g.setColor(Color.green);
+		g.fillRect(10, 20,(int)((Player.life/100) * 250) , 30);		
+		g.setColor(Color.white);
+		g.drawRect(10, 20, 250, 30);
+		g.setFont(new Font("Arial", Font.BOLD,22));
+		g.drawString("Moedas: " +Player.currentCoins+"/"+Player.maxCoins, (Game.WIDTH*Game.SCALE) -150, 30);
+	}
+	
+}
