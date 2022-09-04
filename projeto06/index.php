44a26b05b4e40f5da733c487cb9e793d493ca992
Gallamion <112811519+Gallamion@users.noreply.github.com>
2022-09-04 10:23:33 -0300

    Add files via upload

---
+++ projeto06/index.php
@@ -0,0 +1,170 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0">
+	<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="css/style.css">	
+	<title>Projeto 06</title>
+</head>
+<body>
+	
+	<section class="main">
+		<div class="layer"></div>
+		<div class="center">
+			<header>
+				<div class="icons-social">
+					<a href="https://facebook.com">Facebook</a>
+					<a href="https://instagram.com">Instagram</a>
+				</div><!--icons-social-->
+				<div class="clear"></div>
+				<div class="logo">
+					Gallamion
+				</div><!--logo-->
+				<nav class="desktop">
+					<ul>
+						<li><a href="">Home</a></li>
+						<li><a href="">Sobre</a></li>
+						<li><a href="">Contato</a></li>
+					</ul>
+				</nav><!--desktop-->	
+				<div class="clear"></div>		
+			</header>
+			<div class="conteudo-header">
+				<h2>Bem-vindo ao <span>website!</span></h2>
+				<p>Vestibulum velit nunc, volutpat in ligula nec, vestibulum dictum risus. Quisque convallis ante ipsum, id vestibulum nisi porta luctus. Quisque non massa a risus mollis imperdiet. Nam efficitur vehicula arcu vitae accumsan. Praesent id gravida magna. Nam sollicitudin augue eget ligula malesuada aliquam. Aliquam interdum magna ipsum, sed iaculis leo pharetra nec.</p>
+				<p> Cras ullamcorper vestibulum sapien, ut posuere magna interdum sit amet. Pellentesque imperdiet fermentum ante, ut laoreet nunc viverra ac. Maecenas pharetra, nulla non dignissim semper, nisi justo congue justo, in sagittis odio erat lacinia mi. Nunc vestibulum elit a viverra pharetra.</p>
+				<a href="">Saiba mais</a>
+			</div><!--conteudo-header-->
+		</div><!--center-->
+	</section><!--main-->
+
+
+
+
+
+
+
+
+	<section class="planos">
+		<div class="chamada-planos">
+			<div class="center">
+				<h2>Confira nossos planos</h2>
+			</div><!--center-->
+		</div><!--chamada-planos-->
+		<section class="box-planos">
+			<div class="center">
+				<div class="box-planos-single">
+					<div class="box-planos-wraper">
+					<h2>Lorem ipsum</h2>
+					<div class="pontos-plano">
+						<p>Lorem ipsum silor domor</p>
+						<p>Lorem ipsum silor domor</p>
+						<p>Lorem ipsum silor domor</p>
+						<p>Lorem ipsum silor domor</p>
+					</div><!--pontos-plano-->	
+					<div class="preco">
+						<h2>R$25,00</h2>
+					</div><!--preco-->
+					</div> <!--box-planos-wraper-->				
+				</div><!--box-planos-single-->
+				<div class="box-planos-single">
+					<div class="box-planos-wraper">
+					<h2>Lorem ipsum</h2>
+					<div class="pontos-plano">
+						<p>Lorem ipsum silor domor</p>
+						<p>Lorem ipsum silor domor</p>
+						<p>Lorem ipsum silor domor</p>
+						<p>Lorem ipsum silor domor</p>
+					</div><!--pontos-plano-->	
+					<div class="preco">
+						<h2>R$25,00</h2>
+					</div><!--preco-->
+					</div> <!--box-planos-wraper-->				
+				</div><!--box-planos-single-->
+				<div class="box-planos-single">
+					<div class="box-planos-wraper">
+					<h2>Lorem ipsum</h2>
+					<div class="pontos-plano">
+						<p>Lorem ipsum silor domor</p>
+						<p>Lorem ipsum silor domor</p>
+						<p>Lorem ipsum silor domor</p>
+						<p>Lorem ipsum silor domor</p>
+					</div><!--pontos-plano-->	
+					<div class="preco">
+						<h2>R$25,00</h2>
+					</div><!--preco-->
+					</div> <!--box-planos-wraper-->				
+				</div><!--box-planos-single-->
+			</div><!--center-->
+		</section><!--box-planos-->
+	</section><!--planos-->
+
+
+	<section class="parallax">
+		<div class="overlay-parallax"></div>
+		<div class="center">
+			<h2>Sobre nossa <span style="color: #2692FF;">empresa</span></h2>
+			<p>Vestibulum velit nunc, volutpat in ligula nec, vestibulum dictum risus. Quisque convallis ante ipsum, id vestibulum nisi porta luctus. Quisque non massa a risus mollis imperdiet. Nam efficitur vehicula arcu vitae accumsan. Praesent id gravida magna. Nam sollicitudin augue eget ligula malesuada aliquam. Aliquam interdum magna ipsum, sed iaculis leo pharetra nec.</p>
+			<p>
+			Cras ullamcorper vestibulum sapien, ut posuere magna interdum sit amet. Pellentesque imperdiet fermentum ante, ut laoreet nunc viverra ac. Maecenas pharetra, nulla non dignissim semper, nisi justo congue justo, in sagittis odio erat lacinia mi. Nunc vestibulum elit a viverra pharetra.</p>
+		</div><!--center-->
+	</section><!--parallax-->
+
+
+
+
+
+	<section class="final-section">
+		<div class="center">
+			<div class="w50 contato">
+				<h2><div class="border-efeito"></div>Entre em contato</h2>
+				<form>
+					<input type="text" placeholder="insira seu nome">
+					<input type="email" placeholder="insira seu email">
+					<textarea placeholder="insira sua mensagem"></textarea>
+					<input type="submit" value="Enviar!">
+				</form>
+				</div><!--w50 contato-->
+
+			<div class="w50 time">
+				<h2><div class="border-efeito"></div> Conheça nosso time</h2>
+				<div class="w50 equipe-single">
+					<div class="topo-box-usuario">
+					<div class="box-avatar"></div><!--box-avatar-->
+					<div class="info-usuario">
+						<p style="font-weight: bold;">Matheus Lima</p>
+						<p style="font-weight: italic;">CEO</p>
+					</div><!--info-usuario-->
+				</div><!--topo-box-usuario-->
+				<div class="descricao-usuario-box">
+					<p>interdum sit amet. Pellentesque imperdiet fermentum ante, ut laoreet nunc viverra ac. Maecenas pharetra, nulla non dignissim semper, nisi justo congue justo, in sagittis odio erat lacinia mi. Nunc vestibulum elit a viverra pharetra</p>
+				</div><!--descricao-usuario-box-->
+				</div><!--w50 equipe-single-->
+				
+
+			<div class="w50 equipe-single">
+					<div class="topo-box-usuario">
+					<div class="box-avatar"></div><!--box-avatar-->
+					<div class="info-usuario">
+						<p style="font-weight: bold;">Matheus Lima</p>
+						<p style="font-weight: italic;">CEO</p>
+					</div><!--info-usuario-->
+				</div><!--topo-box-usuario-->
+				<div class="descricao-usuario-box">
+					<p>interdum sit amet. Pellentesque imperdiet fermentum ante, ut laoreet nunc viverra ac. Maecenas pharetra, nulla non dignissim semper, nisi justo congue justo, in sagittis odio erat lacinia mi. Nunc vestibulum elit a viverra pharetra</p>
+				</div><!--descricao-usuario-box-->
+				</div><!--w50 equipe-single-->
+
+			</div><!--w50 time-->
+			<div class="clear"></div>	
+		</div><!--center-->
+	</section><!--final-section-->
+
+	<footer>
+		<p>Todos direitos reservados ao projeto 06</p>
+	</footer>
+
+
+</body>
+</html>
